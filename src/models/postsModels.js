11de24d10d8b0394358663d2cbe57c2dfc431305
Rafael Lopes Ens <rafaellopes.ifrs@gmail.com>
2024-11-21 23:30:58 -0300

    adicionando rotas POSTs

--- src/models/postsModels.js
+++ src/models/postsModels.js
@@ -2,8 +2,14 @@ import conectarAoBanco from '../config/dbConfig.js'
 
 const conexao = await conectarAoBanco(process.env.URL_CONEXAO_BD)
 
-export default async function getTodosPosts(){
+export async function getTodosPosts(){
     const db = conexao.db("Imersao-Instabytes")
     const colecao = db.collection("Posts")
     return colecao.find().toArray()
+}
+
+export async function criarPost(novoPost) {
+    const db = conexao.db("Imersao-Instabytes")
+    const colecao = db.collection("Posts")
+    return colecao.insertOne(novoPost)
 }
